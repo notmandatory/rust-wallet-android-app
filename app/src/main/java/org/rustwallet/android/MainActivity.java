bba78bf9652d93c515f8279ac4d1cf4e038b2bd5
Steve Myers <steve@notmandatory.org>
2019-11-10 18:39:24 -0800

    call getMaster with new mnemonic words

--- app/src/main/java/org/rustwallet/android/MainActivity.java
+++ app/src/main/java/org/rustwallet/android/MainActivity.java
@@ -5,6 +5,9 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Date;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private static String PASSPHRASE = "correct horse battery staple";
@@ -19,7 +22,9 @@ public class MainActivity extends AppCompatActivity {
 
         AccountService accountService = new AccountService();
 
-        MasterAccount master = accountService.getMaster(Entropy.Low, Network.Bitcoin, PASSPHRASE);
+        List<String> mnemonicWords = accountService.getMnemonicWords(Entropy.Low);
+
+        MasterAccount master = accountService.getMaster(mnemonicWords, new Date(), Network.Bitcoin, PASSPHRASE, null);
 
         Account account = accountService.getAccount(master, PASSPHRASE, 0, 0, 0, 0, 10);
 
